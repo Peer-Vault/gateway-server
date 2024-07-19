03ad6f43e5e75b00247ebf32387e6af0c94d7023
Manish Kumar Choudhary <manish@platformcommons.com>
2024-07-20 01:01:49 +0530

    add to discovery and enable gateway

--- src/main/java/com/peer/vault/api_gateway/config/RoutingConfig.java
+++ src/main/java/com/peer/vault/api_gateway/config/RoutingConfig.java
@@ -1,5 +1,6 @@
 package com.peer.vault.api_gateway.config;
 
+import com.peer.vault.api_gateway.filter.AuthenticationFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,11 +10,18 @@ import org.springframework.context.annotation.Configuration;
 public class RoutingConfig {
 
     @Bean
-    public RouteLocator peerVaultConfig(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r -> r.path("/api/file/**")
+    public RouteLocator peerVaultConfig(RouteLocatorBuilder routeLocatorBuilder, AuthenticationFilter authenticationFilter) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/file/**")
+                        .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
                         .uri("lb://FILESERVICE"))
+                .route(p -> p
+                        .path("/user/**")
+                        .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
+                        .uri("lb://USERSERVICE"))
                 .build();
     }
+
 
 }
