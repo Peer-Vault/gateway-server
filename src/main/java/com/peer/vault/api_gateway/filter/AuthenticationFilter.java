4e7f340225b4f09c1b9dff8a09f092ec01c92961
Manish Kumar Choudhary <manish@platformcommons.com>
2024-08-04 00:33:05 +0530

    Route config and CORS

--- src/main/java/com/peer/vault/api_gateway/filter/AuthenticationFilter.java
+++ src/main/java/com/peer/vault/api_gateway/filter/AuthenticationFilter.java
@@ -45,6 +45,37 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
         });
     }
 
+
+//    @Override
+//    public GatewayFilter apply(Config config) {
+//        return ((exchange, chain) -> {
+//            if (validator.isSecured.test(exchange.getRequest())) {
+//                //header contains token or not
+//                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+//                    throw new RuntimeException("missing authorization header");
+//                }
+//
+//                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+//                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+//                    authHeader = authHeader.substring(7);
+//                }
+//                try {
+//                    jwtUtil.validateToken(authHeader);
+//                } catch (Exception e) {
+//                    throw new RuntimeException("un authorized access to application");
+//                }
+//            }
+////            String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+////            System.out.println("Auth Header" + authHeader);
+////            if (authHeader != null) {
+////                return chain.filter(exchange.mutate().request(
+////                        exchange.getRequest().mutate().header(HttpHeaders.AUTHORIZATION, authHeader).build()
+////                ).build());
+////            }
+//            return chain.filter(exchange);
+//        });
+//    }
+
     public static class Config {
 
     }
--- src/main/java/com/peer/vault/api_gateway/filter/RouteValidator.java
+++ src/main/java/com/peer/vault/api_gateway/filter/RouteValidator.java
@@ -12,13 +12,25 @@ public class RouteValidator {
     public static final List<String> openApiEndpoints = List.of(
             "/user/auth/register",
             "/user/auth/login",
-            "/notifications",
+            "/user/auth/validate",
             "/eureka"
     );
 
     public Predicate<ServerHttpRequest> isSecured =
             request -> openApiEndpoints
                     .stream()
-                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+                    .noneMatch(uri -> request.getURI().getPath().startsWith(uri));
+
+//    public static final List<String> openApiEndpoints = List.of(
+//            "/user/auth/register",
+//            "/user/auth/login",
+//            "/notifications",
+//            "/eureka"
+//    );
+//
+//    public Predicate<ServerHttpRequest> isSecured =
+//            request -> openApiEndpoints
+//                    .stream()
+//                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
 
 }
