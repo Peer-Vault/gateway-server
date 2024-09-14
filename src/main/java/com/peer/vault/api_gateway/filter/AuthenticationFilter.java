e89b8ceb5553c46f71ce222edf1bab118b348d7c
Manish Kumar Choudhary <manish@platformcommons.com>
2024-09-14 18:31:23 +0530

    auth controller

--- src/main/java/com/peer/vault/api_gateway/filter/AuthenticationFilter.java
+++ src/main/java/com/peer/vault/api_gateway/filter/AuthenticationFilter.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
+
 import org.springframework.stereotype.Component;
 
 @Component
